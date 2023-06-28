40b3441b7e5088338ee96836108275b1b8096de2
Ria Gupta <74742605+rg663@users.noreply.github.com>
2023-06-27 21:01:19 -0400

    Update apps.py

--- weeklyreportapp/apps.py
+++ weeklyreportapp/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class WeeklyreportappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'coldfront.plugins.weeklyreportapp'
+    name = 'weeklyreportapp'
